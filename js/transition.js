f213e31a5e07b22b1a035fd30fe2f7f7afa4655b
Matthew <matthewoodward01@gmail.com>
2020-06-14 12:15:25 -0400

    Adapted to fit new about page

--- js/transition.js
+++ js/transition.js
@@ -4,8 +4,8 @@ import {TimelineLite} from 'gsap';
 class Fade extends Highway.Transition {
 	in({from, to, done}) {
 		const tl = new TimelineLite();
-		tl.fromTo(to, 0.5, { left: '-100%', top: '50%' }, { left: '0%' })
-			.fromTo(to, 0.5, { height: '2vh' }, { height: '100vh', top: '0%', onComplete: () => {
+		tl.fromTo(to, 0.5, { left: '-100%', top: '50vh' }, { left: '0%' })
+			.fromTo(to, 0.5, { height: '2vh' }, { height: '100vh', top: '0vh', onComplete: () => {
 				from.remove();
 				done();
 			} })
